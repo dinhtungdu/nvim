429e0a0c0785cc587e7b0f92dbd19dcca73ad4e0
Tung Du <dinhtungdu@gmail.com>
2023-08-30 10:54:50 +0700

    update preinstalled language server

--- lua/plugins/mason.lua
+++ lua/plugins/mason.lua
@@ -3,19 +3,17 @@ return {
   opts = {
     ensure_installed = {
       "bash-language-server",
-      "docker-compose-language-service",
-      "dockerfile-language-server",
       "dot-language-server",
       "eslint_d",
       "html-lsp",
       "intelephense",
-      "nginx-language-server",
       "php-debug-adapter",
       "phpstan",
       "prettierd",
       "shellcheck",
       "shfmt",
       "stylua",
+      "tsserver",
     },
   },
 }
