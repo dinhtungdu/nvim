77ea8aac6e21c276de860d1ea1ba5d525d7aa77e
Tung Du <dinhtungdu@gmail.com>
2023-09-05 15:46:46 +0700

    use static so the cursor wont flash

---
+++ lua/plugins/notify.lua
@@ -0,0 +1,15 @@
+return {
+  "rcarriga/nvim-notify",
+  keys = {
+    {
+      "<leader>uN",
+      function()
+        require("telescope").extensions.notify.notify()
+      end,
+      desc = "Notifications history",
+    },
+  },
+  opts = {
+    stages = "static",
+  },
+}
