b0ffdd352b2218b28e16269b6027258c4aebca71
Tung Du <dinhtungdu@gmail.com>
2023-09-05 16:40:16 +0700

    really fix the telescope issue with git status

--- lua/plugins/telescope.lua
+++ lua/plugins/telescope.lua
@@ -1,31 +1,28 @@
-local Util = require("lazyvim.util")
-
-local function is_git_repo()
-  vim.fn.system("git rev-parse --is-inside-work-tree")
-
-  return vim.v.shell_error == 0
-end
-
 local function get_git_root()
   local dot_git_path = vim.fn.finddir(".git", ".;")
   return vim.fn.fnamemodify(dot_git_path, ":h")
 end
 
-local opts = {}
-
-if is_git_repo() then
-  opts = {
-    cwd = get_git_root(),
-  }
-end
-
 return {
   {
     "nvim-telescope/telescope.nvim",
+    tag = "0.1.2",
     keys = {
       -- git
-      { "<leader>gc", Util.telescope("git_commits", opts), desc = "Git commits" },
-      { "<leader>gs", Util.telescope("git_status", opts), desc = "Git status" },
+      {
+        "<leader>gc",
+        function()
+          require("telescope.builtin").git_commits({ cwd = get_git_root() })
+        end,
+        desc = "Git commits",
+      },
+      {
+        "<leader>gs",
+        function()
+          require("telescope.builtin").git_status({ cwd = get_git_root() })
+        end,
+        desc = "Git status",
+      },
     },
     opts = {
       defaults = {
