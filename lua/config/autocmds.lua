9d2b700a81f63df92fd73aab532a9fbcf541c5f8
Tung Du <dinhtungdu@gmail.com>
2023-09-05 22:37:08 +0700

    disable treesitter for large files

--- lua/config/autocmds.lua
+++ lua/config/autocmds.lua
@@ -9,3 +9,16 @@ vim.api.nvim_create_autocmd({ "FileType" }, {
     vim.b.autoformat = false
   end,
 })
+
+vim.api.nvim_create_autocmd("BufReadPost", {
+  pattern = { "*" },
+  callback = function()
+    local filesize = vim.fn.getfsize(vim.fn.expand("%:p"))
+
+    if filesize < 1024 * 1024 then
+      return
+    end
+
+    vim.treesitter.stop()
+  end,
+})
