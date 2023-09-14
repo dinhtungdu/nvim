1c7c2d0a4365680e071717dc9bd7dcd2724c66ef
Tung Du <dinhtungdu@gmail.com>
2023-09-14 09:03:23 +0700

    better indent char

--- init.lua
+++ init.lua
@@ -171,7 +171,7 @@ require('lazy').setup({
     -- Enable `lukas-reineke/indent-blankline.nvim`
     -- See `:help indent_blankline.txt`
     opts = {
-      char = '│',
+      char = '▏',
       show_trailing_blankline_indent = false,
     },
   },
