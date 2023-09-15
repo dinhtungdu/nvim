7a106766c841d6036633f7936bda8fc1c82f89fa
Tung Du <dinhtungdu@gmail.com>
2023-09-15 22:54:16 +0700

    add get root util

--- lua/tung/utils.lua
+++ lua/tung/utils.lua
@@ -1,90 +1,131 @@
 local M = {}
 
 function M.get_git_root()
-  local dot_git_path = vim.fn.finddir(".git", ".;")
-  return vim.fn.fnamemodify(dot_git_path, ":h")
+  local dot_git_path = vim.fn.finddir('.git', '.;')
+  return vim.fn.fnamemodify(dot_git_path, ':h')
 end
 
 function M.toggle_terminal_buffer()
-  if vim.bo.buftype == "terminal" then
-    vim.cmd(":b#")
+  if vim.bo.buftype == 'terminal' then
+    vim.cmd ':b#'
     return
   end
 
-  for _, buffer in ipairs(vim.split(vim.fn.execute ":buffers! t", "\n")) do
-    local match = tonumber(string.match(buffer, "%s*(%d+)"))
-    local is_term = string.match(buffer, "term://")
+  for _, buffer in ipairs(vim.split(vim.fn.execute ':buffers! t', '\n')) do
+    local match = tonumber(string.match(buffer, '%s*(%d+)'))
+    local is_term = string.match(buffer, 'term://')
 
     if is_term then
-      vim.cmd(string.format(":b %s", match))
-      vim.cmd(":startinsert")
+      vim.cmd(string.format(':b %s', match))
+      vim.cmd ':startinsert'
       return
     end
   end
 
   -- if there isn't any terminal buffer opened, create one
-  vim.cmd(string.format("cd %s", M.get_git_root()))
-  vim.cmd("terminal")
-  vim.cmd("startinsert")
+  vim.cmd(string.format('cd %s', M.get_git_root()))
+  vim.cmd 'terminal'
+  vim.cmd 'startinsert'
 end
 
 M.icons = {
   dap = {
-    Stopped = { "󰁕 ", "DiagnosticWarn", "DapStoppedLine" },
-    Breakpoint = " ",
-    BreakpointCondition = " ",
-    BreakpointRejected = { " ", "DiagnosticError" },
-    LogPoint = ".>",
+    Stopped = { '󰁕 ', 'DiagnosticWarn', 'DapStoppedLine' },
+    Breakpoint = ' ',
+    BreakpointCondition = ' ',
+    BreakpointRejected = { ' ', 'DiagnosticError' },
+    LogPoint = '.>',
   },
   diagnostics = {
-    Error = " ",
-    Warn = " ",
-    Hint = " ",
-    Info = " ",
+    Error = ' ',
+    Warn = ' ',
+    Hint = ' ',
+    Info = ' ',
   },
   git = {
-    added = " ",
-    modified = " ",
-    removed = " ",
+    added = ' ',
+    modified = ' ',
+    removed = ' ',
   },
   kinds = {
-    Array = " ",
-    Boolean = " ",
-    Class = " ",
-    Color = " ",
-    Constant = " ",
-    Constructor = " ",
-    Copilot = " ",
-    Enum = " ",
-    EnumMember = " ",
-    Event = " ",
-    Field = " ",
-    File = " ",
-    Folder = " ",
-    Function = " ",
-    Interface = " ",
-    Key = " ",
-    Keyword = " ",
-    Method = " ",
-    Module = " ",
-    Namespace = " ",
-    Null = " ",
-    Number = " ",
-    Object = " ",
-    Operator = " ",
-    Package = " ",
-    Property = " ",
-    Reference = " ",
-    Snippet = " ",
-    String = " ",
-    Struct = " ",
-    Text = " ",
-    TypeParameter = " ",
-    Unit = " ",
-    Value = " ",
-    Variable = " ",
+    Array = ' ',
+    Boolean = ' ',
+    Class = ' ',
+    Color = ' ',
+    Constant = ' ',
+    Constructor = ' ',
+    Copilot = ' ',
+    Enum = ' ',
+    EnumMember = ' ',
+    Event = ' ',
+    Field = ' ',
+    File = ' ',
+    Folder = ' ',
+    Function = ' ',
+    Interface = ' ',
+    Key = ' ',
+    Keyword = ' ',
+    Method = ' ',
+    Module = ' ',
+    Namespace = ' ',
+    Null = ' ',
+    Number = ' ',
+    Object = ' ',
+    Operator = ' ',
+    Package = ' ',
+    Property = ' ',
+    Reference = ' ',
+    Snippet = ' ',
+    String = ' ',
+    Struct = ' ',
+    Text = ' ',
+    TypeParameter = ' ',
+    Unit = ' ',
+    Value = ' ',
+    Variable = ' ',
   },
 }
+
+-- returns the root directory based on:
+-- * lsp workspace folders
+-- * lsp root_dir
+-- * root pattern of filename of the current buffer
+-- * root pattern of cwd
+---@return string
+function M.get_root()
+  ---@type string?
+  local path = vim.api.nvim_buf_get_name(0)
+  path = path ~= '' and vim.loop.fs_realpath(path) or nil
+  ---@type string[]
+  local roots = {}
+  if path then
+    for _, client in pairs(vim.lsp.get_active_clients { bufnr = 0 }) do
+      local workspace = client.config.workspace_folders
+      local paths = workspace and vim.tbl_map(function(ws)
+        return vim.uri_to_fname(ws.uri)
+      end, workspace) or client.config.root_dir and { client.config.root_dir } or {}
+      for _, p in ipairs(paths) do
+        local r = vim.loop.fs_realpath(p)
+        if path:find(r, 1, true) then
+          roots[#roots + 1] = r
+        end
+      end
+    end
+  end
+  table.sort(roots, function(a, b)
+    return #a > #b
+  end)
+  ---@type string?
+  local root = roots[1]
+  if not root then
+    path = path and vim.fs.dirname(path) or vim.loop.cwd()
+    ---@type string?
+    root = vim.fs.find(M.root_patterns, { path = path, upward = true })[1]
+    root = root and vim.fs.dirname(root) or vim.loop.cwd()
+  end
+  ---@cast root string
+  return root
+end
 
 return M
 
