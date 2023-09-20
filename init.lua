fe95bac45e98878729deef1828603b1583b61b3b
Tung Du <dinhtungdu@gmail.com>
2023-09-20 14:51:32 +0700

    set group index to show multiple cmp sources

--- init.lua
+++ init.lua
@@ -717,11 +717,11 @@ cmp.setup {
     end, { 'i', 's' }),
   },
   sources = {
-    { name = 'nvim_lsp' },
-    { name = 'luasnip' },
-    { name = 'path' },
-    { name = 'copilot' },
-    { name = 'emoji' },
+    { name = 'nvim_lsp', group_index = 2 },
+    { name = 'luasnip',  group_index = 2 },
+    { name = 'path',     group_index = 2 },
+    { name = 'copilot',  group_index = 2 },
+    { name = 'emoji',    group_index = 2 },
   },
 }
 
