de5d45b21f7577ec503c465f41b86eb40a4e48ed
Tung Du <dinhtungdu@gmail.com>
2023-09-24 13:26:28 +0700

    save

--- init.lua
+++ init.lua
@@ -421,6 +421,9 @@ vim.keymap.set('n', '<C-j>', '<C-w>j', { desc = 'Go to lower window', remap = tr
 vim.keymap.set('n', '<C-k>', '<C-w>k', { desc = 'Go to upper window', remap = true })
 vim.keymap.set('n', '<C-l>', '<C-w>l', { desc = 'Go to right window', remap = true })
 
+-- Save file
+vim.keymap.set({ 'n', 'i', 'v' }, '<C-s>', '<cmd>w<CR>', { desc = 'Save file' })
+
 -- Remap for dealing with word wrap
 vim.keymap.set('n', 'k', "v:count == 0 ? 'gk' : 'k'", { expr = true, silent = true })
 vim.keymap.set('n', 'j', "v:count == 0 ? 'gj' : 'j'", { expr = true, silent = true })
